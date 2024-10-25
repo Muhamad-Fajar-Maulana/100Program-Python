a89f87d84e18f130c22e6209bfc88f3dde8edcce
= <=>
2024-10-25 09:05:50 +0700

    Giometri

---
+++ Giometri/lingkaran.py
@@ -0,0 +1,11 @@
+# DIBUAT OLEH MUHAMAD FAJAR MAULANA
+# TANGGAL 11-10-2024
+# MEMBUAT PROGRAM LINGKARAN
+
+jari_jari = int(input('Masukkan jari-jari :'))
+
+diameter = jari_jari * 2
+keliling = 2 * 3.14 * jari_jari
+
+print(f'Diameter :{round(diameter)}')
+print(f'Keliling :{round(keliling)}')
