d2d7269b488356088a865aa7d3214e1bf1f04d9c
= <=>
2024-11-22 08:43:16 +0700

    tugas

---
+++ tes/tes 1.py
@@ -0,0 +1,17 @@
+# IBUAT OLEH MUHAMAD FAJAR MAULANA
+# TANGGAL 08-11-2024
+# TES 1
+
+print('='*20)
+print('Rumus konvesi suhu: ')
+print('='*20)
+
+c = float(input('Masukkan celcius: '))
+
+r = (4/5) * c
+f = (9/5) * c + 32
+k = c + 273
+
+print(f'Reamur: {r}')
+print(f'Farenhait: {f}')
+print(f'Kelvin: {k}')
